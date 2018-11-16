45486dd9d22a083305921ae7920296f7f05f0f2f
Michael Wulf <michael.wulf@gmail.com>
2018-11-16 18:09:59 -0500

    Puups

--- OSCMessage.m
+++ OSCMessage.m
@@ -22,6 +22,9 @@ classdef OSCMessage < handle
                 
                 validateattributes(byteArray, {'uint8'}, {'vector'}, 'OSCMessage', 'byteArray');
                 
+                % Get address out of the byteArray
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                
                 % Find position of the slash
                 idSlash = find(byteArray == '/', 1);
                 
@@ -32,28 +35,126 @@ classdef OSCMessage < handle
                 if (idSlash == 1)
                     dataLength = length(byteArray);
                     
-                elseif (idSlash == 5)
+                elseif (idSlash > 1)
+                    % Check if something was sent before the slash -> message lengt?!?
+                    
+                    % Take the bytes before the slash
                     temp = byteArray(1:(idSlash-1));
+                    
+                    % Convert these single bytes to an integer by first
+                    % converting them to a hex-char array...it to hex
                     temp = dec2hex(temp,2);
                     temp = sprintf('%s', temp);
                     dataLength = hex2dec(temp);
                     
-                    payloadLen = length(byteArray-idSlash+1);
+                    msgLen = length(byteArray-idSlash+1);
                     
-                    if ( dataLength > payloadLen )
+                    if ( dataLength > msgLen )
                         warning('Specified data length to high!');
-                        dataLength = payloadLen;
+                        dataLength = msgLen;
                     end
                     
+                    % Remove everything before the slash...
+                    % If a length (or what so ever) is sent before the
+                    % address, the value could also be the ascii number of
+                    % the ',' (TzpeTag)...
                     byteArray = byteArray(idSlash:end);
 
                 else
                     error('Unsopported OSC message format! Slash position > 5?!?');
                 end
                 
+                % Find the TypeTags...
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 idTypeTag = find(byteArray == ',', 1);
-                obj.address = char(byteArray(2:(idTypeTag-1)));
-                obj.address = erase(obj.address, char(0));
+                
+                if ( isempty(idTypeTag) )
+                    % Just address
+                    obj.address = char(byteArray(1:end));
+                    % Remove all zero elements in the string
+                    obj.address = erase(obj.address, char(0));
+                    return;
+                else
+                    % Take out the address of the byte array
+                    obj.address = char(byteArray(2:(idTypeTag-1)));
+                    % Remove possible zero elements...
+                    obj.address = erase(obj.address, char(0));
+                end
+                
+                % Remove everything before the TypeTag...
+                byteArray = byteArray(idTypeTag:end);
+                
+                % Find the first zero element!
+                idFirstZero = find(byteArray == 0, 1);
+                
+                if (idFirstZero == 2)
+                    %no data given!
+                    return;
+                else
+                    moduloValue = mod(idFirstZero, 4);
+                    if ( moduloValue == 0 )
+                        idFirstDataByte = idFirstZero + 1;
+                    else
+                        idFirstDataByte = idFirstZero + (4 - moduloValue) + 1;
+                    end
+                    
+                    if ( idFirstDataByte > length(byteArray) )
+                        error('OSC message has a worng format! TypeTags specified but no data transmitted!');
+                    end
+                end
+                
+                % Get the TypeTags
+                typeTags = byteArray(2:idFirstZero-1);
+                
+                if isempty(typeTags)
+                    error('Error while processing OSC message. OSC message has a wrong format!');
+                end
+                
+                for cntr = 1:1:length(typeTags)
+                    try
+                        obj.typeTagList = [obj.typeTagList {OSCTypes(uint8(typeTags(cntr)))}];
+                    catch
+                        error('Unknown OSC TypeTag %s', typeTags(cntr));
+                    end
+                end
+                
+                
+                byteArray = byteArray(idFirstDataByte:end);
+                
+                for cntr = 1:1:length(obj.typeTagList)
+                    % Get number of remaining bytes
+                    remainingBytes = length(byteArray);
+                    
+                    % Get next OSCTypeTag
+                    currType = obj.typeTagList{cntr};
+                    
+                    % Get Size of TypeTag
+                    numBytes = double(OSCTypeSize(currType.char));
+                    
+                    if (currType ~= OSCTypes.OscString)
+                        if (numBytes > remainingBytes)
+                            error('OSC message frame format error! Not enough bytes left...!');
+                        end
+                        
+                        bytes = byteArray(1:numBytes);
+                        
+                        switch (currType)
+                            case OSCTypes.Int32
+                                
+                            case OSCTypes.Int64
+                            case OSCTypes.Float32
+                        end
+                        
+                    else
+                        nextZero = find(byteArray == 0, 1);
+                        
+                        
+                        
+                    end
+
+                    
+                            
+                end
                 
                 
             elseif ( nargin == 3)
