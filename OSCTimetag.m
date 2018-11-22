0dced19798fc57fb0b9aa764f89f0b3d32827b9a
Michael Wulf <michael.wulf@gmail.com>
2018-11-22 14:03:09 -0500

    qwerty

--- OSCTimetag.m
+++ OSCTimetag.m
@@ -20,8 +20,8 @@ classdef OSCTimetag
             if nargin == 1
                 % One input argument
                 % - String: Call datetime-function
-                % - Double:
-                % - Byte array:
+                % - Double: Days from 0-0-0000 00:00:00 (MATLAB format)
+                % - Byte array: receiving from OSCMessage
                 
                 % Take the input argument
                 argin = varargin{1};
@@ -36,8 +36,11 @@ classdef OSCTimetag
                     catch
                         error('Could not recognize the date/time format of %s! See help of MATLAB-internal function datetime for valid DatStrings!',argin);
                     end
+                    
+                    % Get timestamp for Jan-01-1900 00:00:00
                     tempTimestamp1900 = datetime('01-Jan-1900 00:00:00');
                     
+                    % Calculate difference
                     diffTimestamp = tempTimestamp - tempTimestamp1900;
                     
                     if (diffTimestamp < 0)
@@ -45,26 +48,73 @@ classdef OSCTimetag
                         diffTimestamp = 0;
                     end
                     
+                    % Convert to seconds
                     diffTimestamp = diffTimestamp * 86400;
                     
+                    % full seconds
                     obj.seconds = uint32(floor(diffTimestamp));
                     
-                    frac = modulo(diffTimestamp, 1)
-                        
+                    % fractional seconds
+                    frac = modulo(diffTimestamp, 1);
+                    
+                    % Set fractional part....
+                    obj.fractions = uint32(floor(frac * (2^32)));
                     
                 else
                     if (numel(argin) == 1)
+                        if (isa(argin, 'double'))
+                            % Days since January 0, 0000 (MATLAB timestamps)
+                            
+                            tempTimestamp = argin;
+                            
+                            % Get timestamp for Jan-01-1900 00:00:00
+                            tempTimestamp1900 = datetime('01-Jan-1900 00:00:00');
+                            
+                            % Calculate difference
+                            diffTimestamp = tempTimestamp - tempTimestamp1900;
+                            
+                            if (diffTimestamp < 0)
+                                warning('Specified timestamp lies earlier than Jan-01-1900 00:00:00! Setting OSCTimestamp to zero!');
+                                diffTimestamp = 0;
+                            end
+                            
+                            % Convert to seconds
+                            diffTimestamp = diffTimestamp * 86400;
+                            
+                            % full seconds
+                            obj.seconds = uint32(floor(diffTimestamp));
+                            
+                            % fractional seconds
+                            frac = modulo(diffTimestamp, 1);
+                            
+                            % Set fractional part....
+                            obj.fractions = uint32(floor(frac * (2^32)));
+                            
+                        else
+                            % Unsupported...
+                            error('Unsupported datatype to create an OSCTimestamp!');
+                        end
                     elseif ( (size(argin, 1) == 1) && (size(argin, 2)==8) )
+                        if (isa(argin, 'uint8'))
+                            
+                        else
+                            % Unsupported...
+                            error('Unsupported datatype to create an OSCTimestamp!');
+                        end
                     else
-                        % Unsupported 
+                        % Unsupported...
+                        error('Unsupported datatype to create an OSCTimestamp!');
                     end
-                elseif isa(argin, 'double')
-                    % Days since January 0, 0000 (MATLAB timestamps)
-                    
-                else
-                    % Unsupported
+
                 end
             elseif nargin == 6
+                y    = varargin{1}; % Year
+                m    = varargin{2}; % Month
+                d    = varargin{3}; % Day
+                h    = varargin{4}; % Hour
+                min  = varargin{5}; % Minute
+                sec  = varargin{6}; % Seconds
+                
             elseif nargin == 7
                 y    = varargin{1}; % Year
                 m    = varargin{2}; % Month
@@ -81,10 +131,38 @@ classdef OSCTimetag
             
         end
         
+        function d = toMatlabformat(obj)
+        end
+        
+        function c = toChar(obj)
+            
+            datestr(timestamp,'dd-mm-yyyy HH:MM:SS.FFF');
+        end
+        
         function byteArray = toByteArray(obj)
             %TOBYTEARRAY Converts the OSCTimetag instance into a byte array
             %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+            
+            % Create an empty uint8 array
+            byteArray = uint8(zeros(1,8));
+
+            % Create Bitmasks
+            maskByte1 = uint32(hex2dec('ff000000'));
+            maskByte2 = uint32(hex2dec('00ff0000'));
+            maskByte3 = uint32(hex2dec('0000ff00'));
+            maskByte4 = uint32(hex2dec('000000ff'));
+            
+            % Copy the 32-bit seconds part into the byte array
+            byteArray(1) = uint8(bitshift(bitand(obj.seconds, maskByte1), -24));
+            byteArray(2) = uint8(bitshift(bitand(obj.seconds, maskByte2), -16));
+            byteArray(3) = uint8(bitshift(bitand(obj.seconds, maskByte3),  -8));
+            byteArray(4) = uint8(bitshift(bitand(obj.seconds, maskByte4),   0));
+            
+            % Copy the 32-bit fractions part into the byte array
+            byteArray(5) = uint8(bitshift(bitand(obj.fractions, maskByte1), -24));
+            byteArray(6) = uint8(bitshift(bitand(obj.fractions, maskByte2), -16));
+            byteArray(7) = uint8(bitshift(bitand(obj.fractions, maskByte3),  -8));
+            byteArray(8) = uint8(bitshift(bitand(obj.fractions, maskByte4),   0));
         end
     end
 end
