b82a4dc266f8485a0b6ab4c25a1260fc2f572843
Michael Wulf <michael.wulf@gmail.com>
2022-04-15 16:55:44 -0500

    Added license

--- fp2bin.m
+++ fp2bin.m
@@ -5,15 +5,32 @@ function binary = fp2bin(flpoint, format)
 % x = 3.25;
 % s = fp2bin(x, 'single');
 %
-% --------------------------------------------------------------------------
+% -------------------------------------------------------------------------
 % Author:  Michael Wulf
 %          Washington University in St. Louis
 %          Kepecs Lab
 %
-% Date:    03/16/2022
+% Date:    04/15/2022
 % Version: 1.0.2
 % Github:  https://github.com/Michael-Wulf/OSCMessage
-% ------------------------------------------------------------------------------
+%
+% Copyright (C) 2022 Michael Wulf
+%
+% This program is free software; you can redistribute it and/or
+% modify it under the terms of the GNU General Public License
+% as published by the Free Software Foundation; either version 2
+% of the License, or (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+% MA  02110-1301, USA.
+% -------------------------------------------------------------------------
 
 if ( nargin < 1)
     error('No floating point number give!');
